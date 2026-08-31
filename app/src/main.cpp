c3ce00703c868ed7e37a657a621679c4dc99040e
Mircea <26766475+mirceastoian@users.noreply.github.com>
2026-08-31 21:15:43 +0300

    feat: example leds overlay

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,8 +2,8 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
-/* The devicetree node identifier for the "led0" alias. */
-#define LED_NODE DT_ALIAS(led0)
+/* The devicetree node identifier for the "warning_led" alias. */
+#define LED_NODE DT_ALIAS(error_led)
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
---
+++ docs/module_04.md
@@ -0,0 +1,7 @@
+# Module 04
+
+## Build
+
+```
+west build -- -DEXTRA_DTC_OVERLAY_FILE="boards/leds.overlay"
+```
