fd1d6d26dd57995a09879ee9c6d6d3aff2cc2898
Mircea <26766475+mirceastoian@users.noreply.github.com>
2026-09-20 23:30:21 +0300

    feat: led driver on off

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,28 +1,26 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/drivers/sensor.h>
 
-/* The devicetree node identifier for the "app_led" alias. */
-#define LED_NODE DT_ALIAS(app_led)
-
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
+// LED as a sensor device
+const struct device *led = DEVICE_DT_GET(DT_NODELABEL(my_led));
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 int main(void)
 {
-    bool led_state = true;
-
-    if (!gpio_is_ready_dt(&led)) return 0;
-
-    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
+    if (!device_is_ready(led)) {
+        printk("My LED not ready!\n");
+        return -1;
+    }
 
     while (1) {
-        if (gpio_pin_toggle_dt(&led) < 0) return 0;
-
-        led_state = !led_state;
-        LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
+        sensor_sample_fetch(led);
+        k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);
+        sensor_channel_get(led, SENSOR_CHAN_ALL, NULL);
         k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);
     }
+
     return 0;
 }
---
+++ modules/my-led-driver/drivers/my-led-driver/my_led.c
@@ -0,0 +1,84 @@
+#include <zephyr/device.h>
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
+
+#define DT_DRV_COMPAT mircea_my_led_driver
+
+LOG_MODULE_REGISTER(my_led_driver, LOG_LEVEL_INF);
+
+struct my_led_driver_config {
+    struct gpio_dt_spec gpio;
+};
+
+struct my_led_driver_data {
+    bool led_state;
+};
+
+static int my_led_driver_init(const struct device *dev)
+{
+    const struct my_led_driver_config *cfg = (const struct my_led_driver_config *)dev->config;
+    const struct gpio_dt_spec *led = &cfg->gpio;
+
+    if (!gpio_is_ready_dt(led)) return -1;
+    if (gpio_pin_configure_dt(led, GPIO_OUTPUT_INACTIVE) < 0) return -2;
+
+	return 0;
+}
+
+static int control_led(const struct device *dev, const bool state)
+{
+    const struct my_led_driver_config *cfg = (const struct my_led_driver_config *)dev->config;
+    const struct gpio_dt_spec *led = &cfg->gpio;
+    
+    if (gpio_pin_set_dt(led, state) < 0) return -1;
+
+    struct my_led_driver_data *data = dev->data;
+    data->led_state = gpio_pin_get_dt(led);
+
+    return 0;
+}
+
+// ON
+static int my_led_driver_sample_fetch(const struct device *dev,
+                                      enum sensor_channel chan)
+{
+    (void)chan;
+    return control_led(dev, 1);
+}
+
+// OFF
+static int my_led_driver_channel_get(const struct device *dev,
+                                     enum sensor_channel chan,
+                                     struct sensor_value *val)
+{
+    (void)chan;
+    (void)val;
+    return control_led(dev, 0);
+}
+
+static const struct sensor_driver_api my_led_driver_api = {
+	.sample_fetch = my_led_driver_sample_fetch,
+	.channel_get = my_led_driver_channel_get
+};
+
+#define DEVICE_INSTANCE(inst) \
+\
+const static struct my_led_driver_config my_led_driver_##inst##_cfg = { \
+		.gpio = GPIO_DT_SPEC_INST_GET(inst, gpios) \
+}; \
+\
+static struct my_led_driver_data my_led_driver_##inst##_drvdata = { \
+		.led_state = false, \
+}; \
+\
+DEVICE_DT_INST_DEFINE(inst, \
+		my_led_driver_init, \
+		NULL, \
+		&my_led_driver_##inst##_drvdata, \
+		&my_led_driver_##inst##_cfg, \
+		POST_KERNEL, \
+        CONFIG_GPIO_INIT_PRIORITY, \
+		&my_led_driver_api);
+
+DT_INST_FOREACH_STATUS_OKAY(DEVICE_INSTANCE);
